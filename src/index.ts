74a691f4266f5d5c67f97cbbc734115da9088dd8
Austin McGee <947888+amcgee@users.noreply.github.com>
2021-03-15 12:55:08 +0000

    feat: setup dev script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-const express = require( "express" );
+import express from "express"
+
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.PORT || 9000; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
